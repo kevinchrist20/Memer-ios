2b6e654619feadea9b01eeb1663cad86f24b5e6f
kevinchrist20 <kevinamevor@outlook.com>
2021-11-14 21:04:24 +0000

    Initial Commit

---
+++ Memer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memer
+//
+//  Created by Kevin on 14/11/2021.
+//  Copyright © 2021 kevinchrist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
